22dd759e9d218d70df8189b4183b00b294bef3b3
Denys <levchenkoden02@gmail.com>
2025-02-15 23:21:49 +0200

    add:error page

--- components/Loader.vue
+++ components/Loader.vue
@@ -3,5 +3,5 @@ import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
 </script>
 
 <template>
-  <DotLottieVue autoplay loop src="https://lottie.host/4978a09c-6a8e-4af4-befc-dced3a7b4de3/ne9EcNiBtZ.lottie" />
+  <DotLottieVue class="text-white" autoplay loop src="https://lottie.host/4978a09c-6a8e-4af4-befc-dced3a7b4de3/ne9EcNiBtZ.lottie" />
 </template>
