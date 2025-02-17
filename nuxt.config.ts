d1b6f7bb1d67110fcb632c26ee4110551e4f1446
Denys <levchenkoden02@gmail.com>
2025-02-17 12:20:12 +0200

    fix:chunks and config settings about error page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
-  generate: {
-    routes: ["/404"],
-  },
 
   runtimeConfig: {
     mongodbUri: process.env.MONGO_URI,
